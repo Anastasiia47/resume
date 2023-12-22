d90b4ec1c58881a2d6ee7b5205fbe39dab4913bf
Anastasiia47 <nastyaa.ivanovaa47@gmail.com>
2023-12-22 15:22:07 +0200

    load

--- src/route/index.js
+++ src/route/index.js
@@ -23,8 +23,84 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
-  })
+    title: "Resume",
+
+    name: {
+      firstname: 'Ivan',
+      lastname: 'Ivanov',
+    },
+
+    position: "Junior Fullstack JS Developer",
+
+    salary: "600$ в месяц",
+
+    summary:{
+      title: "Summary",
+      text: {
+        value: 'Open-minded for new technologies with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good teamplayer, every colleague is a friend to me.',
+      },
+    },
+
+    expierence:{
+      title: "Other experience",
+      text: {
+        value: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics tournament position, goals etc), analyzing by simple mathematics models and preparing probabilit for such events like: money line - first win / draw / second win, totals etc.'
+      },
+    },
+
+    social:{
+      email: {
+        text: "ivanov@mail.com",
+        href: "mailto:dmytro@mail.com",
+      },
+       phone:{
+        text: "+380670000123",
+        href: "tel:+380670000123",
+      },
+      linkedin:{
+        text:"LinkedIn",
+        href:"//www.linkedin.com/in/dmytro-test",
+      },
+     }
+ })
 })
+
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    title: "Resume",
+
+    name: {
+      firstname: 'Ivan',
+      lastname: 'Ivanov',
+    },
+
+    position: "Junior Fullstack JS Developer",
+
+    salary: "600$ в месяц",
+
+    social:{
+      email: {
+        text: "ivanov@mail.com",
+        href: "mailto:dmytro@mail.com",
+      },
+       phone:{
+        text: "+380670000123",
+        href: "tel:+380670000123",
+      },
+      linkedin:{
+        text:"LinkedIn",
+        href:"//www.linkedin.com/in/dmytro-test",
+      },
+     }
+ })
+})
+
 
 // ================================================================
 
